20eccb0852797b898f5af74d88c70ae1b8e4d93d
Escherwes <45395167+Escherwes@users.noreply.github.com>
2018-11-29 10:13:22 -0500

    Created new Method
    Created a new method that returns an int that was used to make an arraylist.

--- SharingLists.java
+++ SharingLists.java
@@ -1,5 +1,36 @@
+import java.util.ArrayList;
 public class SharingLists{
+
+	public static int reconstructInteger(ArrayList<Integer> list){
+
+		int multiply = 1;
+
+		int addList = 0;
+
+		int temp = 0;
+
+		for(int i = list.size()-1; i>=0; i--){
+
+			temp = list.remove(i);
+
+			addList += temp*multiply;
+
+			multiply *= 10;
+		}
+
+		return addList;
+
+	}
+
 	public static void main(String[]args){
-	
+
+		ArrayList<Integer> list = new ArrayList<>();
+
+		list.add(8);
+		list.add(9);
+		list.add(5);
+		list.add(0);
+
+		System.out.println(reconstructInteger(list));
 	}
 }

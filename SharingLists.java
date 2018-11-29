d20e9fe53570ef384b45c369e6a798d51a279823
Anshika Nangia <anshikanan526@gmail.com>
2018-11-29 10:15:35 -0500

    created method
    convert to array list

--- SharingLists.java
+++ SharingLists.java
@@ -1,5 +1,11 @@
 public class SharingLists{
 	public static void main(String[]args){
-	
 	}
+	//Partner B's 1st Method:
+	ArrayList<Integer> abcd = new ArrayList<>();
+	public static ArrayList <Integer> convertToArrayList(int num){
+	}
+
+
+}
 }
